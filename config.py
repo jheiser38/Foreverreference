436725379a254cb49f983675aee2337635e42cb1
jheiser38 <jheiser38@gmail.com>
2020-07-27 21:50:57 -0400

    Updated mail server

--- config.py
+++ config.py
@@ -23,11 +23,11 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", 'backup if variable is empty') # Gets either
     APP_ADMIN = os.environ.get("APP_ADMIN","jheiser38@gmail.com")
     MAIL_SERVER = os.environ.get("MAIL_SERVER", 'smtp.googlemail.com') # using because gmail
-    MAIL_PORT = os.environ.get('MAIL_PORT','25')  # must use port 587 eith TLS
+    MAIL_PORT = os.environ.get('MAIL_PORT','587')  # must use port 587 with TLS
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS','true').lower() in \
         ['true','on','1']       # returns binary if in the list
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'jheiser38.b@gmail.com')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD','BillyWeiner69^(')
+    MAIL_USERNAME = 'jheiser38.b@gmail.com'# os.environ.get('MAIL_USERNAME', 'jheiser38.b@gmail.com')
+    MAIL_PASSWORD = 'BillyWeiner69^(' #os.environ.get('MAIL_PASSWORD','BillyWeiner69^(')
     MAIL_PREFIX = os.environ.get('MAIL_PREFIX','App Name')
     MAIL_SENDER = 'Admin <jheiser38.b@gmail.com>' # This is just how it displays in the email
     SQLALCHEMY_TRACK_MODIFICATIONS = False      # Tracks changes to rows
